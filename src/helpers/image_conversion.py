28a8f5c6b72a52444ba86f346ed3c9c304b0286b
thinkpaste <crunchpaste@users.noreply.github.com>
2025-01-27 22:43:27 +0200

    Speedup of QPixmap generation

--- src/helpers/image_conversion.py
+++ src/helpers/image_conversion.py
@@ -1,7 +1,9 @@
 from PIL import Image
 import numpy as np
 from PIL.ImageQt import ImageQt
-from PySide6.QtGui import QPixmap
+from PySide6.QtGui import QPixmap, QImage
+
+import time
 
 def pixmap_to_numpy(pixmap):
     image = pixmap.toImage()
@@ -11,12 +13,40 @@ def pixmap_to_numpy(pixmap):
 
     return image_array
 
-def numpy_to_pixmap(image_array):
+def numpy_to_pixmap(img_array):
+
+    image_array = np.copy(img_array)
     if image_array.dtype != np.uint8:
+        # This one is quite slow for large images and definitely needs improvement
         image_array = (image_array * 255).clip(0, 255).astype(np.uint8)
-    pil_image = Image.fromarray(image_array)
-    pil_image.convert("RGBA")
-    qimage = ImageQt(pil_image)
+    if image_array.ndim == 2:
+        # This is the case only when the halftoning mode is None
+        format = QImage.Format_Grayscale8
+        h, w = image_array.shape
+        c = 1
+    elif image_array.ndim == 3:
+        h, w, c = image_array.shape
+        if c == 2:
+            # This is grayscale with alpha
+            # For some reason QImage does not support LA so this is needed
+            image_array = np.dstack((
+                image_array,
+                image_array,
+                image_array,
+                np.full((h, w), 255, dtype=np.uint8)
+            ))
+            format = QImage.Format_RGBA8888
+            c = 4
+        elif c == 3:
+            # This is RGB without alpha. Just adapt the format
+            format = QImage.Format_RGB888
+        elif c == 4:
+            # This is RGB with alpha. Just adapt the format
+            format = QImage.Format_RGBA8888
+
+    bytes = c * w
+    qimage = QImage(image_array, w, h, bytes, format)
+    # TODO: Should check how to create the pixmap directly from an array
     pixmap = QPixmap.fromImage(qimage)
 
     return pixmap
