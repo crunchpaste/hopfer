fe42d20a2957dbc789d94945b2ad1a9996ee4e0b
thinkpaste <crunchpaste@users.noreply.github.com>
2025-01-29 04:52:38 +0200

    Speed improvement of thresholding

--- src/algorithms/threshold.py
+++ src/algorithms/threshold.py
@@ -5,18 +5,10 @@ def threshold(img, settings):
     value = settings["threshold_value"] / 100
     return thresh(img, value)
 
-@njit
-def thresh(img, threshold_value=.5):
-    # Apply thresholding
-    output_img = img.copy()
-    h,w = img.shape
-    # output = image.flatten()
-    for i in range(h):
-        for j in range(w):
-            if output_img[i, j] > threshold_value:
-                output_img[i, j] = 1
-            else:
-                output_img[i, j] = 0
-    # thresholded_image = np.where(image >= threshold_value, 1, 0)
-
-    return output_img
+@njit(cache=True)
+def thresh(img, threshold_value):
+    # As simple as it gets :)
+    # Slightly (about 15%) improves performace when in njit. Unfortunately slightly slower in static.
+    t = threshold_value - 0.5
+    img = np.rint(img + t)
+    return img
