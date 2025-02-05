44aa61e22b8a32b4ff26c927fbf24bd20f6cac09
thinkpaste <crunchpaste@users.noreply.github.com>
2025-02-05 20:46:51 +0200

    Fix import bug

--- src/algorithms/thresholdc.py
+++ src/algorithms/thresholdc.py
@@ -1,8 +1,13 @@
-from .static import thresh, sauvola, phansalkar
+from .static import thresh, sauvola, phansalkar, niblack
 
 def threshold(img, settings):
     value = settings["threshold_value"] / 100
     return thresh(img, value)
+
+def niblack_threshold(img, settings):
+    block_size = int(settings["block_size"])
+    k = settings["k_factor"] / 100
+    return niblack(img, block_size, k)
 
 def sauvola_threshold(img, settings):
     block_size = int(settings["block_size"])
@@ -16,5 +21,4 @@ def phansalkar_threshold(img, settings):
     k = settings["k_factor"] / 100
     p = settings["p_factor"] / 100
     q = settings["q_factor"] / 10
-
     return phansalkar(img, block_size, dynamic_range, k, p, q)
