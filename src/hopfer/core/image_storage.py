50d42de4e909e71ad20a4b4735b1e07a33e3c8b6
thinkpaste <crunchpaste@users.noreply.github.com>
2026-01-08 02:06:07 +0200

    Fixed saving when None

--- src/hopfer/core/image_storage.py
+++ src/hopfer/core/image_storage.py
@@ -371,7 +371,7 @@ class ImageStorage(QObject):
                 self.processed_image, self.color_dark, self.color_light
             )
         else:
-            image = self.processed_image.astype(np.uint8) * 255
+            image = self.processed_image.astype(np.uint8)
 
         if self.ignore_alpha or self.alpha is None:
             output_image = image
